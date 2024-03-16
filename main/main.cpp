8c16218c4c4a0131e6c605f0e1911007b0661856
spikeyamk <spikeyamk@github.com>
2024-03-16 14:05:19 +0100

    adding .gitignore, fixing submodules

--- main/main.cpp
+++ main/main.cpp
@@ -5,27 +5,21 @@
 #include "sd_card.hpp"
 #include "lfs_port/sdmmc/test.hpp"
 
-esp_err_t all_block_test() {
-    SD_Card& sd_card { SD_Card::get_instance() };
-
-    /*
+esp_err_t all_block_test(SD_Card& sd_card) {
     const esp_err_t ret { sd_card.check_all_blocks() };
     if(ret != ESP_OK) {
         return ret;
     }
-    */
-
-    sd_card.deinit();
     return ESP_OK;
 }
 
-void littlefs_test() {
-    SD_Card& sd_card { SD_Card::get_instance() };
+void littlefs_test(SD_Card& sd_card) {
     Trielo::trielo<LFS::SDMMC::test>(Trielo::OkErrCode(ESP_OK), &sd_card.card);
-    sd_card.deinit();
 }
 
 extern "C" void app_main() {
+    SD_Card& sd_card { SD_Card::get_instance() };
     //Trielo::trielo<all_block_test>(Trielo::OkErrCode(ESP_OK));
     littlefs_test();
+    sd_card.deinit();
 }
